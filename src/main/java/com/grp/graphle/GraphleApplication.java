98accdc75fb9a81bd3599fc115f2c3704e5f4063
iyjaa <izramulyaputra@gmail.com>
2026-07-31 16:07:27 +0700

    Setup GitHub Actions build workflow

---
+++ src/main/java/com/grp/graphle/GraphleApplication.java
@@ -0,0 +1,13 @@
+package com.grp.graphle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grp/graphle/GraphleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grp.graphle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
